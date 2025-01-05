0f27548ef6f055bef92778947f787c2a03caa465
malobs <malobamercy958@gmail.com>
2025-01-05 20:42:34 +0300

    linking react to redux

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
-import store from "./store.js"
+import store from "./store.js";
 
-store.dispatch({type:"deposit", payload:250})
-console.log(store.getState())
+store.dispatch({ type: "deposit", payload: 250 });
+console.log(store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
