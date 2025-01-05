4f9c8f27853ac7ebbd25ec259af28bcdba9f4d8a
malobs <malobamercy958@gmail.com>
2025-01-05 23:40:01 +0300

    setting up store with rtk

--- src/store.js
+++ src/store.js
@@ -1,16 +1,14 @@
-import { applyMiddleware, combineReducers, createStore } from "redux";
 import accountReducer from "./features/accounts/accountSlice";
 import customerReducer from "./features/customers/customerSlice";
-import { thunk } from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
-const rootReducer = combineReducers({
-  account: accountReducer,
-  customer: customerReducer,
-});
 
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(thunk))
-);
+import { configureStore } from "@reduxjs/toolkit";
+
+const store = configureStore({
+  reducer: {
+    account: accountReducer,
+    customer: customerReducer,
+  },
+  // Enable Redux DevTools in development mode
+});
 
 export default store;
